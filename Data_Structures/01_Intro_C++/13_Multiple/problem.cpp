d2b50157c3b02b26fb5c6d28a4130be5fc50008f
prakaran <prakaranbhatt@gmail.com>
2026-06-13 07:39:56 +0545

    Is Multiple or not

---
+++ Data_Structures/01_Intro_C++/13_Multiple/problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    if(m % n == 0){
+        cout << "Yes" << endl;
+
+    }else{
+        cout << "No" << endl;
+    }
+}
