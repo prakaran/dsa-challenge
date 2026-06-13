22773949b766395ef6b24c1e08b8b882a375d88f
prakaran <prakaranbhatt@gmail.com>
2026-06-13 07:45:30 +0545

    pass or fail

---
+++ Data_Structures/01_Intro_C++/14_Pass_Or_Fail/problem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int marks;
+    cin >> marks;
+    if(marks >= 35){
+        cout << "Pass" << endl;
+    }else{
+        cout << "Fail" << endl;
+    }
+}
