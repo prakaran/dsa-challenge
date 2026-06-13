406be91a48a97222fef5ac8f6e056349c7740f2c
prakaran <prakaranbhatt@gmail.com>
2026-06-13 07:22:19 +0545

    Calculator, Calculator II and Add last digits

---
+++ Data_Structures/01_Intro_C++/09_Calculator_II/problem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main(){
+    long long n;
+    long long m;
+    cin >> n;
+    cin >> m;
+    cout << n << " + " << m << " = " << n+m << endl;
+    cout << endl;
+    cout << n << " - " << m << " = " << n-m << endl;
+    cout << endl;
+    cout << n << " * " << m << " = " << n*m << endl;
+    cout << endl;
+    cout << n << " / " << m << " = " << n/m << endl;
+    cout << endl;
+    cout << n << " % " << m << " = " << n%m << endl;
+    return 0;
+}
