836ca42bbafceb0cf606c63e1efa328e228f5a1e
prakaran <prakaranbhatt@gmail.com>
2026-06-18 06:25:02 +0545

    x pow n

---
+++ Data_Structures/02_Loops/10_x_pow_n/problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x, n;
+    cin >> x >> n;
+    long long result=1;
+    for(int i =1; i<=n; i++ ){
+        result *= x;
+    }
+    cout << result << endl;
+    return 0;
+}
