1130c62386c9bc237acb53f4cb5bc5989c973885
prakaran <prakaranbhatt@gmail.com>
2026-06-20 11:11:30 +0545

    print hollow vertical triangle

--- Data_Structures/03_Pattern_Printing/11_Hollow_Vertical_Triangle/problem.cpp
+++ Data_Structures/03_Pattern_Printing/11_Hollow_Vertical_Triangle/problem.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    int n; 
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            if(j == 1 || j == i){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+            if( i != j){
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    for(int i = n-1; i>=1; i--){
+        for(int j = i; j>=1; j--){
+            if( i != j){
+                cout << " ";
+            }
+            if(j == 1 || j == i){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
