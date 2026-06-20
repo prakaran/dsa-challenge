58e120c445ea1c64b7a55529774ce651ef343a20
prakaran <prakaranbhatt@gmail.com>
2026-06-20 07:21:07 +0545

    2 stars

--- Data_Structures/03_Pattern_Printing/01_2-Stars/problem.cpp
+++ Data_Structures/03_Pattern_Printing/01_2-Stars/problem.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    int rows; 
+    cin >> rows;
+    for(int i=0; i<rows; i++){
+        cout << "**" << endl;
+    }
 }
