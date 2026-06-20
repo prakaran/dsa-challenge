f9da12fd909fde4c5dcfb299b35aac7cec2e07ab
prakaran <prakaranbhatt@gmail.com>
2026-06-20 08:18:36 +0545

    print pyramid

--- Data_Structures/03_Pattern_Printing/06_Pyramid/problem.cpp
+++ Data_Structures/03_Pattern_Printing/06_Pyramid/problem.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    int n; 
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
