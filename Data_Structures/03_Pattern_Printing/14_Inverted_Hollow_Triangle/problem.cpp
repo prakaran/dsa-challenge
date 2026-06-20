c52982dacd46785ade86df893076e4ec15bfaaf4
prakaran <prakaranbhatt@gmail.com>
2026-06-20 12:32:22 +0545

    print inverted hollow triangle

--- Data_Structures/03_Pattern_Printing/14_Inverted_Hollow_Triangle/problem.cpp
+++ Data_Structures/03_Pattern_Printing/14_Inverted_Hollow_Triangle/problem.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+    for(int i = n; i>=1; i--){
+        for(int k = n-i; k>=0; k--){
+            cout << " ";
+        }
+        for(int j = i; j>=1; j--){
+
+            if( i != j){
+                cout << " ";
+            }
+            if(i == j || j == 1 || i == n ){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
