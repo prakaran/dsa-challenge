99dfe9ecdc513fa5fcdec1a5f6ff0fff5ade6034
prakaran <prakaranbhatt@gmail.com>
2026-06-20 09:08:15 +0545

    print vertical triangle

--- Data_Structures/03_Pattern_Printing/10_Vertical_Traingle/problem.cpp
+++ Data_Structures/03_Pattern_Printing/10_Vertical_Traingle/problem.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << "*";
+            if(j!=i){
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    for(int i = n-1; i>=1; i--){
+        for(int j = i; j>=1; j--){
+            if(i!=j){
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
