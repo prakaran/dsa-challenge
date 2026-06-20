8dcbd5324125fc7e752a04556139522be83eff02
prakaran <prakaranbhatt@gmail.com>
2026-06-20 08:22:57 +0545

    print inverse pyramid

--- Data_Structures/03_Pattern_Printing/07_Inverse_Pyramid/problem.cpp
+++ Data_Structures/03_Pattern_Printing/07_Inverse_Pyramid/problem.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+    for(int i = n; i>=1; i--){
+        for(int j = i; j>=1; j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
