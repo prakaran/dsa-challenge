83b83a38166bae79a57d5474bea75a6cfd1ff7f8
prakaran <prakaranbhatt@gmail.com>
2026-06-20 15:53:23 +0545

    print hollow diamond pattern

--- Data_Structures/03_Pattern_Printing/16_Hollow_Diamond/problem.cpp
+++ Data_Structures/03_Pattern_Printing/16_Hollow_Diamond/problem.cpp
@@ -1,6 +1,68 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
+//     for(int i = 1; i<=n; i++){
+//         for(int k = n-i; k>=1;k--){
+//             cout << " ";
+//         }
+//         for(int j = 1; j<=i; j++){
+//             if(j == 1 || j == i){
+//                 cout << "*";
+//             }else{
+//                 cout << " ";
+//             }
+//             if(i != j){
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+//     for(int i = n-1; i>=1; i--){
+//         for(int k = 1; k<=n-i; k++){
+//             cout << " ";
+//         }
+//         for(int j = i; j>=1; j--){
+//             if(i != j){
+//                 cout << " ";
+//             }
+//             if(j == 1 || j == i){
+//                 cout << "*";
+//             }else{
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+#include<iostream>
 using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= 2*n-1; i++){
+        int stars = (i<=n)? i : (2*n-i); 
+        // int spaces = (i<=n)? n-i : i-n;
+        int spaces = n - stars;
+
+        for(int s =1; s<=spaces; s++){
+            cout << " ";
+        }
+        for(int k = 1; k<=stars; k++){
+            if(k == 1 || k == stars ){
+                cout << "*"; 
+            }else{
+                cout << " ";
+            }
+            if(i!=k){
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
