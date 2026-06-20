eeb5f30b400816531abfde35a3698f34445930d5
prakaran <prakaranbhatt@gmail.com>
2026-06-20 07:59:33 +0545

    print hollow square

--- Data_Structures/03_Pattern_Printing/04_Hollow_Square/problem.cpp
+++ Data_Structures/03_Pattern_Printing/04_Hollow_Square/problem.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+
+
+    // for(int i = 1; i<=n; i++){
+    //     for(int j = 1; j <= n; j++){
+    //         if(i == 1 || i == n){
+    //             cout << "*";
+    //         }else{
+    //             if( j == 1 || j == n){
+    //                 cout << "*";
+    //             }else{
+    //                 cout << " ";
+    //             }
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<n;j++){
+            if(i == 0 || i == n-1 || j == 0 ){
+                cout << "*";
+            }else if(j == n-1){
+                cout << setw(n-1) << "*"; 
+            }
+            
+        }
+        cout << endl;
+    }
     return 0;
 }
