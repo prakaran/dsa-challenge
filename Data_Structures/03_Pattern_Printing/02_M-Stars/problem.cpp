73fae407e8a7043a28ff35cf90b4543f453e1440
prakaran <prakaranbhatt@gmail.com>
2026-06-20 07:25:33 +0545

    m stars

--- Data_Structures/03_Pattern_Printing/02_M-Stars/problem.cpp
+++ Data_Structures/03_Pattern_Printing/02_M-Stars/problem.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    int rows, stars;
+    cin >> rows >> stars;
+    for(int i = 0; i<rows; i++){
+        for(int j=0; j<stars; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
