dd91808d3b43376b961a3267be44b8c049037e9c
prakaran <prakaranbhatt@gmail.com>
2026-06-23 08:18:30 +0545

    print factors

--- Data_Structures/05_Functions/02_Print_Factors_I/problem.cpp
+++ Data_Structures/05_Functions/02_Print_Factors_I/problem.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+void printFactors(int n){
+    for(int i =1; i<=n; i++){
+        if(n%i == 0){
+            cout << i;
+            if(i != n){
+                cout << " ";
+            }
+        }
+    }
+    cout << "\n";
+}
 int main() {
-    cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+    printFactors(n);
     return 0;
 }
